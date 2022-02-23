e491c400762f8d227158ef4ada0315d54b6376d1
Kiddmac <ronaldphw@gmail.com>
2022-02-23 14:06:35 -0500

    Se termino de generar las funciones para el triángulo y para el círculo

--- figuras.js
+++ figuras.js
@@ -33,7 +33,7 @@ console.group('Triangulo')
 // console.log('La altura del triángulo mide:' + alturaTriangulo + 'cm');
 
 function perimetroTriangulo(lado1, lado2, base) {
-    return lado1 + lado2 + base;
+      return (lado1 + lado2 + base);
 } 
 
 function areaTriangulo(base, altura) {
@@ -87,4 +87,48 @@ function calcularAreaCuadrado(){
 
     const area = areaCuadrado(value);
     alert(area);
+}
+
+function calcularPerimetroTriangulo(){
+    const lado1 = document.getElementById("lado1");
+    const value1 = Number (lado1.value);
+
+    const lado2 = document.getElementById('lado2')
+    const value2 = Number (lado2.value);
+
+    const base = document.getElementById('base')
+    const valueBase = Number (base.value);
+
+    const perimetro = perimetroTriangulo(value1, value2, valueBase);
+
+    alert(perimetro);
+}
+function calcularAreaTriangulo(){
+
+    const base = document.getElementById('base')
+    const valueBase = Number (base.value);
+
+    const altura = document.getElementById("altura")
+    const valueAltura = Number(altura.value);
+
+    const area = areaTriangulo(valueBase, valueAltura);
+
+    alert(area);
+}
+
+function calcularDiametro() {
+    const radio = document.getElementById("InputRadio");
+    const valueRadio = Number(radio.value);
+
+    const diametro = diametroCirculo(valueRadio)
+    alert(diametro)
+}
+
+function calcularArea() {
+    const radio = document.getElementById("InputRadio");
+    const valueRadio = Number(radio.value);
+
+    const area = areaCirculo(valueRadio);
+
+    alert(area)
 }
